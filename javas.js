cd27f72932bda33a68034fe85eeb433242156175
Egor <yaiba637@gmail.com>
2023-06-19 12:12:58 +0400

    update progect

--- javas.js
+++ javas.js
@@ -1,5 +1,18 @@
 'use strict';
-a=5;
-console.log(a)
-let s=true;
-console.log(!s);
+let money = prompt("Ваш бюджет на месяц?", '')
+    time = prompt("Введите дату в формате YYYY-MM-DD", '');
+let appData = {
+    budget: momey,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", '')
+    a2 = prompt("Во сколько обойдется?", '')
+    a3 = prompt("Введите обязательную статью расходов в этом месяце", '')
+    a4 = prompt("Во сколько обойдется?", '');
+appData.expenses.a1=a2;
+appData.expenses.a3=a4;
+alert(appData.budget / 30);
