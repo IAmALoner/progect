fe910ba12bb5c75000602d9d4d368e7f7e5990a2
Egor <yaiba637@gmail.com>
2023-06-25 21:06:36 +0400

    stage 2 in program

---
+++ js.js
@@ -0,0 +1,30 @@
+// for (let i = 0; i<2;i++){
+//     let a = prompt("Введите обязательную статью расходов в этом месяце", '')
+//         b = prompt("Во сколько обойдется?", '');
+//     if ((typeof(a))==='string' && (typeof(a)) != null && (typeof(b)) != null && a!='' && b!='' && a.length <50) {
+//         appData.expenses[a]=b;
+//     } else {
+//         i--;
+//     }
+// let i=0;
+// while (i<2) {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце", '')
+//         b = prompt("Во сколько обойдется?", '');
+//         if ((typeof(a))==='string' && (typeof(a)) != null && (typeof(b)) != null && a!='' && b!='' && a.length <50) {
+//             appData.expenses[a]=b;
+//         } else {
+//             i--;
+//         }
+//         i++;
+// }
+// let i=0;
+// do {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце", '')
+//         b = prompt("Во сколько обойдется?", '');
+//     if ((typeof(a))==='string' && (typeof(a)) != null && (typeof(b)) != null && a!='' && b!='' && a.length <50) {
+//     } else {
+//         i--
+//     }
+//     i++;
+// }
+// while (i<2);
